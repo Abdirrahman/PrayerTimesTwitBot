b3d7ac794f955b6a8a2ce6637570d777a077d5da
Abdirrahman Mohamed <abdirrahmang@gmail.com>
2020-08-30 10:46:50 +0100

    run time

--- Bot.py
+++ Bot.py
@@ -4,6 +4,8 @@ import json
 import requests
 from datetime import datetime
 import arrow
+import sched
+import time
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -11,23 +13,20 @@ load_dotenv()
 
 url = "https://www.londonprayertimes.com/api/times/?format=json&key=8e9e14c7-323f-4755-88d7-8bd51ab9f094&year=2020&month=august"
 r = requests.get(url)
-print(r)
+
 
 text_json = json.loads(r.text)
-print(text_json)
+
 today = arrow.now().format('YYYY-MM-DD')
+timed = datetime.now().strftime('%I:%M')
+s = sched.scheduler(time.time, time.sleep)
 
-time = datetime.now().strftime('%I:%M')
 
-print(text_json["times"][today]["fajr"])
 fajr = text_json["times"][today]["fajr"]
 dhuhr = text_json["times"][today]["dhuhr"]
 asr = text_json["times"][today]["asr"]
 magrib = text_json["times"][today]["magrib"]
 isha = text_json["times"][today]["isha"]
-
-
-print(text_json["times"][today]["fajr"])
 
 
 consumer_key = os.getenv('CONSUMER_KEY')
@@ -42,21 +41,21 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(key, secret)
 
 api = tweepy.API(auth)
-if time == fajr:
-    api.update_status(text_json["times"][today]["fajr"])
 
 
-if time == dhuhr:
-    api.update_status(text_json["times"][today]["dhuhr"])
+def check_pt(sc):
+    if timed == fajr:
+        api.update_status(text_json["times"][today]["fajr"])
+    elif timed == dhuhr:
+        api.update_status(text_json["times"][today]["dhuhr"])
+    elif timed == asr:
+        api.update_status(text_json["times"][today]["asr"])
+    elif timed == magrib:
+        api.update_status(text_json["times"][today]["margib"])
+    elif timed == isha:
+        api.update_status(text_json["times"][today]["isha"])
+    s.enter(60, 1, check_pt, (s,))
 
 
-if time == asr:
-    api.update_status(text_json["times"][today]["asr"])
-
-
-if time == magrib:
-    api.update_status(text_json["times"][today]["margib"])
-
-
-if time == isha:
-    api.update_status(text_json["times"][today]["isha"])
+s.enter(60, 1, check_pt, (s,))
+s.run()
